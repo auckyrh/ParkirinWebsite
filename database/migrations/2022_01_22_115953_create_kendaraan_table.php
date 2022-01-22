55f8fc0224cb65cd249ca66d5cdb42f87d034339
Aucky <auckyrh@gmail.com>
2022-01-22 19:07:20 +0700

    initiate project

---
+++ database/migrations/2022_01_22_115953_create_kendaraan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKendaraanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kendaraan', function (Blueprint $table) {
+            $table->string("plat_nomor")->primary();
+            $table->foreignId('id')->constrained('users')->onDelete('cascade');
+            $table->string("jenis_kendaraan");
+            $table->string("merk_model");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kendaraan');
+    }
+}
